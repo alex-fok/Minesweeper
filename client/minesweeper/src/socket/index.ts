a4bcf83118ffa3c538b640f1567bbe4a81625d7d
Alex Fok <foktmalexander@gmail.com>
2023-05-20 18:18:32 -0700

    Socket message name change

--- client/minesweeper/src/socket/index.ts
+++ client/minesweeper/src/socket/index.ts
@@ -3,7 +3,7 @@ const socket = new WebSocket("ws://localhost:8080/ws")
 
 const socketEvents: Record<string, (event: any)=>void> = {}
 socket.addEventListener("open", _ => {
-    socket.send(JSON.stringify({name: 'newGame'})) 
+    socket.send(JSON.stringify({name: 'newRoom'})) 
 })
 socket.addEventListener("message", event => {
     const { name, content } = JSON.parse(event.data)
