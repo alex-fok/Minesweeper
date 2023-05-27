91d25663bc8ec24028207b8756875314d4897774
Alex Fok <foktmalexander@gmail.com>
2023-05-27 11:41:07 -0700

    Add env for dev environment

--- client/minesweeper/src/socket/index.ts
+++ client/minesweeper/src/socket/index.ts
@@ -1,5 +1,6 @@
-
-const socket = new WebSocket('ws://localhost:8080/ws')
+// For prod: window.local.hostname
+// For dev : ws://localhost:8080/ws
+const socket = new WebSocket(import.meta.env.VITE_SERVER ? import.meta.env.VITE_SERVER : window.location.hostname + '/ws')
 
 const socketEvents: Record<string, (event: any)=>void> = {}
 socket.addEventListener('open', _ => {
@@ -7,8 +8,8 @@ socket.addEventListener('open', _ => {
 })
 socket.addEventListener('message', event => {
     const { name, content } = JSON.parse(event.data)
-    // console.log('name: ', name)
-    // console.log('content: ', content)
+    console.log('name: ', name)
+    console.log('content: ', content)
    if (!socketEvents[name]) return
    socketEvents[name](JSON.parse(content))
 })
