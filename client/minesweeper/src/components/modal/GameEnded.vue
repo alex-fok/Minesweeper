c40a0a2fd4ceacc1065c7b9550ccd005efca9a3f
Alex Fok <foktmalexander@gmail.com>
2023-06-11 22:29:33 -0700

    Add end game modal for non-players

--- client/minesweeper/src/components/modal/GameEnded.vue
+++ client/minesweeper/src/components/modal/GameEnded.vue
@@ -10,10 +10,21 @@ const isPlayer = gameState.players[gameState.id] !== undefined
 const isWon = isPlayer && gameState.winner === gameState.id
 </script>
 <template>
-    <div class='modal-item'>
-        <div class='modal-end-game'>{{ isWon ? 'You Won!' : 'You Lost!' }}</div>
-    </div>
-    <div class='modal-close' @click='close()'>&#10005;</div>
+    <template v-if='isPlayer'>
+        <div class='modal-item'>
+            <div class='modal-end-game'>{{ isWon ? 'You Won!' : 'You Lost!' }}</div>
+        </div>
+        <div class='modal-item'>
+            <span class='btn' @click=''>REMATCH?</span>
+        </div>
+        <div class='modal-close' @click='close()'>&#10005;</div>
+    </template>
+    <template v-else>
+        <div class='modal-item'>
+            {{ gameState.players[gameState.winner].alias }} Won!
+        </div>
+        <div class='modal-close' @click='close()'>&#10005;</div>
+    </template>
 </template>
 <style scoped>
     @import '@/assets/modal.css';
