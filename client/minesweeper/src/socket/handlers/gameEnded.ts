392ae4894c3064ffc3caf055c5d124a318dc1b48
Alex Fok <foktmalexander@gmail.com>
2023-06-15 06:51:27 -0700

    Use displayContent method

--- client/minesweeper/src/socket/handlers/gameEnded.ts
+++ client/minesweeper/src/socket/handlers/gameEnded.ts
@@ -11,8 +11,7 @@ export default (data: GameEnded) => {
     const { winner } = data
     gameState.status  = END,
     gameState.winner = winner
-    uiState.modal.isActive = true
-    uiState.modal.content = 'gameEnded'
+    uiState.modal.displayContent('gameEnded')
     if (gameState.isPlayer) 
         media.play(winner === gameState.id ? 'win' : 'lose')
 }
