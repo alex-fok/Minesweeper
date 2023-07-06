9154734b94f76c9d494405304c5a0f34b88c1f60
Alex Fok <foktmalexander@gmail.com>
2023-07-06 16:54:10 -0700

    Add player offline message. Close #6

--- client/minesweeper/src/socket/handlers/playerOffline.ts
+++ client/minesweeper/src/socket/handlers/playerOffline.ts
@@ -1,6 +1,7 @@
-import { gameState } from '@/store'
+import { gameState, uiState } from '@/store'
 
 export default (data: { client: string }) => {
     const { client } = data
     gameState.players[client].isOnline = false
+    uiState.modal.displayContent('message', `Player ${gameState.players[client].alias} is offline`)
 }
