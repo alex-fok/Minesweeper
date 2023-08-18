12c6089a8e45e86b7d4d6afd4652569ad0447afb
Alex Fok <foktmalexander@gmail.com>
2023-08-18 09:06:11 -0700

    Remove comment

--- client/minesweeper/src/socket/handlers/shared/updateCounter.ts
+++ client/minesweeper/src/socket/handlers/shared/updateCounter.ts
@@ -1,7 +1,5 @@
 import { gameState } from '@/store'
 export default (score: Record<string, number>, bombsLeft: number) => {
-    // gameState.players[gameState.].score = playerScore
-    // gameState.opponent.score = opponentScore
     const playerIds = Object.keys(score)
     playerIds.forEach(id => {
         gameState.players[id].score = score[id]
